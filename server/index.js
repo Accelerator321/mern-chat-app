34e42575caf6db1877e7893601fe411c3f516a89
Accelerator321 <shyamprime2610@gmail.com>
2024-03-04 20:23:41 +0530

    origin fix5

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const {Server} = require('socket.io');
 
 const io = new Server(80,{
-    cors: true
+    cors: {
+        origin: 'https://accelerator321.github.io',
+      }
 
 });
 const socketToemail = new Map();
